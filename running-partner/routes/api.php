5b5a15b746eccaa34431084b7e59cbbe714c234d
anasavic35 <anasavic35@gmail.com>
2024-01-01 17:24:47 +0100

    Pristup rutama samo autentifikovanim korisnicima

--- running-partner/routes/api.php
+++ running-partner/routes/api.php
@@ -69,7 +69,7 @@ Route::get('/statistika-trke', [StatistikaTrkeController::class, 'index']);
 /*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});*/
+});
 
 
 
@@ -86,5 +86,60 @@ Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::resource('/plan_trke', PlanTrkeController::class)->only(['store','update','destroy']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+*/
 
+// ...
 
+Route::prefix('planovi-trka')->group(function () {
+    // Dodajte middleware za autorizaciju na sve rute unutar ovog prefixa
+    Route::middleware(['auth:sanctum'])->group(function () {
+        // Prikazuje sve planove trka
+        Route::get('/', [PlanTrkeController::class, 'index']);
+        // Prikazuje planove trka na osnovu id
+        Route::get('/{id}', [PlanTrkeController::class, 'show']);
+        // Dodaje novi plan trke
+        Route::post('/', [PlanTrkeController::class, 'store']);
+        // Ažurira plan trke
+        Route::put('/{id}', [PlanTrkeController::class, 'update']);
+    });
+});
+
+Route::prefix('trkaci')->group(function () {
+    // Dodajte middleware za autorizaciju na sve rute unutar ovog prefixa
+    Route::middleware(['auth:sanctum'])->group(function () {
+        //Prikazuje listu svih trkaca
+        Route::get('/', [TrkacController::class, 'index']);
+        //Prikazuje profil odredjenog trkača na osnovu id
+        Route::get('/{id}', [TrkacController::class, 'show']);
+        //Dodaje novog trkaca
+        Route::post('/', [TrkacController::class, 'store']);
+        //Azurira informacije na profilu trkaca
+        Route::put('/{id}', [TrkacController::class, 'update']);
+        //Brise odredjenog trkaca iz baze
+        Route::delete('/{id}', [TrkacController::class, 'destroy']);
+    });
+});
+
+Route::prefix('komentari')->group(function () {
+    // Dodajte middleware za autorizaciju na sve rute unutar ovog prefixa
+    Route::middleware(['auth:sanctum'])->group(function () {
+        // Prikazuje sve komentare
+        Route::get('/', [KomentarController::class, 'index']);
+        // Dodaje novi komentar
+        Route::post('/', [KomentarController::class, 'store']);
+        // Brise odredjeni komentar
+        Route::delete('/{id}', [KomentarController::class, 'destroy']);
+    });
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/statistika-trke', [StatistikaTrkeController::class, 'store']);
+    Route::get('/statistika-trke', [StatistikaTrkeController::class, 'index']);
+    Route::resource('/komentar', KomentarController::class)->only(['store','destroy']);
+    Route::resource('/statistika_trke', StatistikaTrkeController::class)->only(['update', 'destroy']);
+    Route::resource('/trkac', TrkacController::class)->only(['store','update','destroy']);
+    Route::resource('/plan_trke', PlanTrkeController::class)->only(['store','update','destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+// ...
