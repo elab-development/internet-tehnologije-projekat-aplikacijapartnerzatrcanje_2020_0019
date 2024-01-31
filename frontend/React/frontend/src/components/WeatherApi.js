4d779ada839da22cc9f910bce5d18f4331382aa4
ivanaillic <ii20200019@student.fon.bg.ac.rs>
2024-01-31 18:25:15 +0100

    Stilizovanje WeatherApi komponente

--- frontend/React/frontend/src/components/WeatherApi.js
+++ frontend/React/frontend/src/components/WeatherApi.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "./Button";
 
 const api = {
   key: "5ac80301d03ad3ec7d0928bd7a84fcd4",
@@ -22,22 +23,53 @@ function WeatherApi() {
   };
 
   return (
-    <div className="Weather">
+    <div className="Weather" style={{ padding: "20px", fontSize: "20px" }}>
       <header className="Weather-header">
-        <h2 style={{ textAlign: "center" , color:"white"}}>Proveri da li je vreme pogodno za trčanje:</h2>
+        <h2 style={{ textAlign: "center", color: "white" }}>
+          Proveri da li je vreme pogodno za trčanje:
+        </h2>
 
-        <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center", 
+            marginTop: "20px",
+          }}
+        >
           <input
             type="text"
             placeholder="Unesite željeni grad"
+            style={{
+              fontSize: "18px",
+              padding: "8px",
+              marginRight: "5px", 
+              width: "250px", 
+              height: "40px", 
+            }}
             onChange={(e) => setSearch(e.target.value)}
           />
-          <button onClick={searchPressed}>Pretraži</button>
+          
+         
+          <Button
+            type="button"
+            onClick={searchPressed}
+            buttonStyle="btn--primary"
+            buttonSize="btn--large"
+          >
+            Pretraži
+          </Button>
         </div>
 
         {typeof weather.main !== "undefined" ? (
-          <div style={{ textAlign: "center", marginTop: "20px", color: "white" }}>
-          
+          <div
+            style={{
+              textAlign: "center",
+              marginTop: "20px",
+              color: "white",
+              fontSize: "20px",
+            }}
+          >
             <p>{weather.name}</p>
             <p>{weather.main.temp}°C</p>
             <p>{weather.weather[0].main}</p>
