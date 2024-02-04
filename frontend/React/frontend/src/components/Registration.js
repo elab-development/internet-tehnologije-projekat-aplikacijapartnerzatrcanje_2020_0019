672a7e8cacb50691ab1bc0dfc1fa4c6d0386f1db
anasavic35 <anasavic35@gmail.com>
2024-02-04 14:06:27 +0100

    Izmenjena komponenta Registration

--- frontend/React/frontend/src/components/Registration.js
+++ frontend/React/frontend/src/components/Registration.js
@@ -4,10 +4,12 @@ import './Registration.css';
 
 const Registration = () => {
   const [formData, setFormData] = useState({
-    name: '',
+    ime: '',
+    prezime: '',
+    datum_rodjenja: '',
+    pol: '',
     email: '',
     password: '',
-    role: 'user',
   });
 
   const handleInputChange = (e) => {
@@ -17,15 +19,26 @@ const Registration = () => {
   const handleRegistration = async () => {
     try {
       const response = await axios.post('http://127.0.0.1:8000/api/register', formData);
-      console.log(response.data); 
+      const responseData = response.data;
+  
+      // Provera uspešnosti registracije
+      if (responseData && responseData.data) {
+        console.log('Registracija uspešna:', responseData.data);
+        // Dodatne akcije koje želite preduzeti nakon uspešne registracije
+      } else {
+        console.log('Neuspešna registracija. Server vraća:', responseData);
+      }
     } catch (error) {
-      console.error(error);
+      console.error('Greška prilikom registracije:', error);
     }
   };
 
   return (
     <div className="registration-container"> 
-      <input type="text" name="name" placeholder="Full Name" onChange={handleInputChange} />
+      <input type="text" name="ime" placeholder="Ime" onChange={handleInputChange} />
+      <input type="text" name="prezime" placeholder="Prezime" onChange={handleInputChange} />
+      <input type="date" name="datum_rodjenja" placeholder="Datum rođenja" onChange={handleInputChange} />
+      <input type="text" name="pol" placeholder="Pol" onChange={handleInputChange} />
       <input type="email" name="email" placeholder="Email" onChange={handleInputChange} />
       <input type="password" name="password" placeholder="Password" onChange={handleInputChange} />
       <button className="registration-button" onClick={handleRegistration}>Register</button> 
